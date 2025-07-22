e49a84665f1515d8fdbd60d53753964f39ea18a0
zhongziyule <97587732+zhongziyule@users.noreply.github.com>
2025-07-22 13:56:13 +0800

    adjust导入五次测试

--- Package.swift
+++ Package.swift
@@ -1,6 +1,5 @@
 // swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-// swift-tools-version: 6.1
 
 import PackageDescription
 
@@ -12,40 +11,38 @@ let package = Package(
             targets: ["oneSDK"]),
     ],
     dependencies: [
-        .package(
-            url: "https://github.com/facebook/facebook-ios-sdk.git",
-            from: "14.1.0"),
-        .package(
-            url: "https://github.com/google/GoogleSignIn-iOS.git",
-            from: "8.0.0"),
-        .package(
-            url: "https://github.com/adjust/ios_sdk.git",
-            from: "5.4.0")
+        .package(url: "https://github.com/facebook/facebook-ios-sdk.git", from: "14.1.0"),
+        .package(url: "https://github.com/google/GoogleSignIn-iOS.git", from: "8.0.0"),
+        .package(url: "https://github.com/adjust/ios_sdk.git", from: "5.4.0")
     ],
     targets: [
         .binaryTarget(
             name: "oneSDKBranch",
             url: "https://github.com/zhongziyule/onesdk-ios/raw/main/oneSDK.xcframework.zip",
-            checksum: "3743c484fa35d44560320b7fe00cf722c57acc8fc7568254e0369e2c9110db0d"
+            checksum: "0ef0170c5596d1bfab4cb453f29c47a48298d27b708b211f6544b3df480b14cc"
         ),
         .target(
             name: "oneSDK",
             dependencies: [
                 "oneSDKBranch",
+                // Facebook 依赖
                 .product(name: "FacebookAEM", package: "facebook-ios-sdk"),
                 .product(name: "FacebookBasics", package: "facebook-ios-sdk"),
                 .product(name: "FacebookCore", package: "facebook-ios-sdk"),
                 .product(name: "FacebookLogin", package: "facebook-ios-sdk"),
                 .product(name: "FacebookShare", package: "facebook-ios-sdk"),
                 .product(name: "FacebookGamingServices", package: "facebook-ios-sdk"),
+                // Adjust 依赖
                 .product(name: "AdjustGoogleOdm", package: "ios_sdk"),
                 .product(name: "AdjustSdk", package: "ios_sdk"),
                 .product(name: "AdjustWebBridge", package: "ios_sdk"),
+                // Google Sign-In 依赖
                 .product(name: "GoogleSignIn", package: "GoogleSignIn-iOS"),
                 .product(name: "GoogleSignInSwift", package: "GoogleSignIn-iOS")
             ],
+            // 仅在 iOS 平台添加必要的 unsafeFlags，并限制范围
             linkerSettings: [
-                .unsafeFlags(["-ObjC", "-all_load"])
+                .unsafeFlags(["-ObjC"], .when(platforms: [.iOS]))
             ]
         ),
         .testTarget(
@@ -54,3 +51,4 @@ let package = Package(
         ),
     ]
 )
+    
